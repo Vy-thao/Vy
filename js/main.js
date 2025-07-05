263a503ef5f87d2552e11ae938988ba275066627
Vy-thao <ahhhh65rt@gmail.com>
2025-07-05 17:55:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U BANGARAM').split('')
+    const titles = ('tình yêu của tôi,anh yêu em').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
